9276e618ba4db15b3f58192f81036da207318f4b
jay45652 <jay45652@gmail.com>
2023-04-18 13:53:26 +0900

    2023-04-18

---
+++ pre1-1/src/Test2.java
@@ -0,0 +1,9 @@
+public class Test2 {
+    public boolean solution(int num) {
+        boolean answer = true;
+        if (num % 4 == 1) {
+            answer = false;
+        }
+        return answer;
+    }
+}
